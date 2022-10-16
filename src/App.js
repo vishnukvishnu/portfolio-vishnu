716d61e4fdf35024ddc796f1e3f6e379e5c5dfe5
vishnukvishnu <vishnulogan4@gmail.com>
2022-10-16 11:21:53 +0530

    remove svg

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { useState, createContext } from 'react';
 
 export const containerContext = createContext('');
 const App = () => {
-  const [refVal, setRef] = useState({ home: '', about: '', service: '' });
+  const [refVal, setRef] = useState({ home: 0, about: 0,projects:0, service: 0 });
   return (
     <>
-      <containerContext.Provider value={{ refVal, setRef }}>
+      <containerContext.Provider value={{ refVal:refVal, setRef:setRef }}>
         <PortfolioWrapper />
       </containerContext.Provider>
     </>

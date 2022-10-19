5a9832cf3f39c86966adc97c8585498713029c43
vishnukvishnu <vishnulogan4@gmail.com>
2022-10-19 21:51:23 +0530

    testing changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className="col-12 col-md-7 left-home-wrapper">
         <div className="wrapper">
           <h4 className="header-2 header-mb-2 header-mb-2 mb-4">
-            WELCOME TO MY PORTFOLIO!
+            WELCOME TO MY PORTFOLIO!!
           </h4>
           <h1 className="header-1 header-mb-1 header-sm-mt-2">VISHNU K</h1>
           <div className="programming-wrapper">

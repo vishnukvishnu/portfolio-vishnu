93e13c83c1bf7af08d87433ea55eae517ede6c36
viskk <viskk@VISKK-X40-J.in.oracle.com>
2022-10-29 11:21:16 +0530

    mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import logo from '../assets/logo.png';
+import { useState } from 'react';
 
 const Header = ({ scrollFn }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleOnChange = (section) => {
+    scrollFn(section);
+    setIsChecked(!isChecked);
+  };
   return (
     <div className="header-wrapper">
       <nav
@@ -43,28 +49,44 @@ const Header = ({ scrollFn }) => {
           <div className="left-nav-mobile-wrapper display-block">
             <img src={logo} alt="logo" />
           </div>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onChange={handleOnChange}
+            checked={isChecked}
+          />
           <span className="icon"></span>
           <span className="icon"></span>
           <span className="icon"></span>
 
           <ul id="menu" className="right-nav-wrapper flex-vtkl-center">
-            <li className="navigation-link" onClick={() => scrollFn('home')}>
+            <li
+              className="navigation-link"
+              onClick={() => handleOnChange('home')}
+            >
               <i className="fa fa-home-alt"></i>HOME
             </li>
-            <li className="navigation-link" onClick={() => scrollFn('about')}>
+            <li
+              className="navigation-link"
+              onClick={() => handleOnChange('about')}
+            >
               <i className="fa fa-user"></i>ABOUT
             </li>
             <li
               className="navigation-link"
-              onClick={() => scrollFn('projects')}
+              onClick={() => handleOnChange('projects')}
             >
               <i className="fa fa-project-diagram"></i>PROJECTS
             </li>
-            <li className="navigation-link" onClick={() => scrollFn('service')}>
+            <li
+              className="navigation-link"
+              onClick={() => handleOnChange('service')}
+            >
               <i className="fa fa-code"></i>SERVICES
             </li>
-            <li className="navigation-link" onClick={() => scrollFn('contact')}>
+            <li
+              className="navigation-link"
+              onClick={() => handleOnChange('contact')}
+            >
               <i className="fa fa-phone-square-alt"></i>CONTACT
             </li>
             <li className="navigation-link width-100">
